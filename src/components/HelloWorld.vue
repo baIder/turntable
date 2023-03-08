f6170b713840772d6f7f2a183b9dab88ce64b920
王云阳 <511763880@qq.com>
2023-03-08 17:00:03 +0800

    完成转动

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,33 @@
 <template>
     <div class="root">
         <div class="menu-wrapper">
-            <div class="menu data-area">
-            <span
-                    v-for="(item,index) in areaArr"
-                    :key="item.id"
-                    @click="handleAreaClick(item.id,$event)"
-                    :style="{rotate:`${19*index}deg`}"
-                    class="text-wrapper"
-            >
-                <span>
-                {{ item.name }}
+            <div class="menu data-area" ref="dataArea">
+                <span
+                        v-for="(item) in areaArr"
+                        :key="item.id"
+                        @click="handleAreaClick(item.id,$event)"
+                        class="text-wrapper"
+                >
+                    <span>
+                        {{ item.name }}
+                    </span>
                 </span>
-            </span>
             </div>
-            <div class="menu data-pos">
-            <span
-                    v-for="(item,index) in currentArea.children"
-                    :key="item.id"
-                    @click="handlePosClick(item.id,$event)"
-                    :style="{rotate:`${19*index}deg`}"
-                    class="text-wrapper"
-            >
-                <span>
-                {{ item.name }}
+            <div class="menu data-pos" ref="dataPos">
+                <span
+                        v-for="(item,index) in currentArea.children"
+                        :key="item.id"
+                        @click="handlePosClick(item.id,$event)"
+                        :style="{rotate:`${19*index}deg`}"
+                        :data-rotate="19*index"
+                        class="text-wrapper"
+                >
+                    <span
+                            :data-rotate="19*index"
+                    >
+                        {{ item.name }}
+                    </span>
                 </span>
-            </span>
             </div>
             <div class="controller controller-lv1"/>
             <div class="controller controller-lv2"/>
@@ -52,16 +54,60 @@ export default {
             this.currentArea = this.areaArr.find(i => i.id === id)
             this.currentPos = this.currentArea.children[0]
             toggleActive({ev})
+            this.$nextTick(() => {
+                this.$refs.dataPos.children[0].classList.add('active')
+                this.calcPosDeg()
+            })
+            this.$refs.dataArea.style.rotate = '-' + ev.target.dataset.rotate + 'deg'
+            this.$refs.dataPos.style.rotate = '0deg'
         },
         handlePosClick(id, ev) {
             this.currentPos = this.currentArea.children.find(i => i.id === id)
             toggleActive({ev})
+            this.$refs.dataPos.style.rotate = -Number(ev.target.dataset.rotate) + 'deg'
+        },
+        calcPosDeg() {
+            const posArr = Array.from(this.$refs.dataPos.children)
+            posArr.forEach((el, index) => {
+                if (index === 0) {
+                    el.children[0].dataset.rotate = '0'
+                    el.dataset.rotate = '0'
+                } else {
+                    const deg = Number(posArr[index - 1].dataset.rotate) + (this.currentArea.children[index].name.length + this.currentArea.children[index - 1].name.length) * 1.5 + 3
+                    el.children[0].dataset.rotate = deg.toString()
+                    el.dataset.rotate = deg.toString()
+                    el.style.rotate = deg + 'deg'
+                }
+            })
+        },
+        calcAreaDeg() {
+            const areaArr = Array.from(this.$refs.dataArea.children)
+            areaArr.forEach((el, index) => {
+                if (index === 0) {
+                    el.children[0].dataset.rotate = '0'
+                    el.dataset.rotate = '0'
+                } else {
+                    const deg = Number(areaArr[index - 1].dataset.rotate) + (this.areaArr[index].name.length + this.areaArr[index - 1].name.length) * 1.5
+                    el.children[0].dataset.rotate = deg.toString()
+                    el.dataset.rotate = deg.toString()
+                    el.style.rotate = deg + 'deg'
+                }
+            })
+        },
+        init() {
+            this.areaArr = mockData()
+            this.currentArea = this.areaArr[0]
+            this.currentPos = this.currentArea.children[0]
+            this.$nextTick(() => {
+                this.$refs.dataArea.children[0].classList.add('active')
+                this.$refs.dataPos.children[0].classList.add('active')
+                this.calcAreaDeg()
+                this.calcPosDeg()
+            })
         },
     },
     mounted() {
-        this.areaArr = mockData()
-        this.currentArea = this.areaArr[0]
-        this.currentPos = this.currentArea.children[0]
+        this.init()
     }
 }
 </script>
@@ -93,6 +139,7 @@ export default {
     border-radius: 50% 50% 0 0;
     -webkit-transform-style: preserve-3d;
     transform-origin: 50% 100%;
+    transition: all 750ms ease-in-out;
 
     > .text-wrapper {
       position: absolute;
@@ -111,16 +158,29 @@ export default {
         font-size: 1.6vh;
         cursor: pointer;
         border-radius: 40px;
-        border: 1px solid red;
         z-index: 200;
         pointer-events: visible;
         transform: translateY(-3px);
         display: inline-block;
+        position: relative;
       }
 
       > &.active {
         > span {
           color: white;
+          font-weight: 700;
+
+          &::after {
+            content: "";
+            position: absolute;
+            width: 18px;
+            height: 2px;
+            background: rgba(255, 255, 255, .8);
+            border-radius: 110px 0 110px 0;
+            left: 50%;
+            transform: translateX(-50%);
+            bottom: 3px;
+          }
         }
       }
     }
--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,7 @@
 export const mockData = () => {
     const areaArr = []
     for (let i = 1; i < 13; i++) {
-        const area = {id: i, name: `地区${i}`, children: []}
+        const area = {id: i, name: `地区${Math.random().toFixed(Math.ceil(Math.random() * 5))}`, children: []}
         for (let j = 1; j < Math.ceil(Math.random() * 8) + 1; j++) {
             area.children.push({id: i * 10 + j, name: `方位${j}`})
         }
